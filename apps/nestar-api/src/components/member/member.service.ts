e9fa75030c18ae8dd5a87125e3ffed083a6597c0
odilboooy10 <odilkhon318@gmail.com>
2024-05-26 01:43:41 +0900

    feat: develop createProperty graphQL API

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -7,7 +7,7 @@ import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
-import { T } from '../../libs/types/common';
+import { StatisticModifier, T } from '../../libs/types/common';
 import { ViewService } from '../view/view.service';
 import { ViewGroup } from '../../libs/enums/view.enum';
 
@@ -151,5 +151,19 @@ export class MemberService {
 		const result: Member = await this.memberModel.findOneAndUpdate({ _id: input._id }, input, { new: true }).exec();
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 		return result;
+	}
+
+	public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
+		console.log('executed');
+		const { _id, targetkey, modifier } = input;
+		return await this.memberModel
+			.findOneAndUpdate(
+				_id,
+				{
+					$inc: { [targetkey]: modifier },
+				},
+				{ new: true },
+			)
+			.exec();
 	}
 }
